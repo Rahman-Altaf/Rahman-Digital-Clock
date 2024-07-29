d84721d174c012f3439fc77b2269dae8b2e8c595
Rahman-Altaf <rahmanch.311@gmail.com>
2024-07-29 10:19:19 +0500

    DIGITAL CLOCK PROJECT 1

---
+++ P1-DG.js
@@ -0,0 +1,27 @@
+async function codexclock() {
+  const time = new Date();
+
+  try {
+    console.log("Project-1 MERN 19")
+    console.log(`Codex Clock: ${time}`);
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    })
+      .then((resolve) => {
+        resolve = time;
+        console.log(resolve);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        console.log("Clock Is Ticking Every Second");
+      });
+    codexclock();
+  } catch (error) {
+    console.log(error);
+  }
+}
+codexclock();
